d7ff55bd3f79131dbd0c9adffc8b35ffe2a3f11f
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2023-07-14 15:57:29 +0200

    iOS: drop Firebase, add Apple APN
    Firebase can't correctly send VoIP notifications

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.20
 
 require (
 	firebase.google.com/go v3.13.0+incompatible
+	github.com/dgraph-io/badger/v4 v4.1.0
 	github.com/gin-gonic/gin v1.9.1
+	github.com/go-co-op/gocron v1.30.1
+	github.com/sideshow/apns2 v0.23.0
 )
 
 require (
@@ -16,30 +19,42 @@ require (
 	cloud.google.com/go/longrunning v0.5.0 // indirect
 	cloud.google.com/go/storage v1.31.0 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/dgraph-io/ristretto v0.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.1 // indirect
+	github.com/golang/glog v1.1.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/flatbuffers v1.12.1 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/s2a-go v0.1.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.5 // indirect
 	github.com/googleapis/gax-go/v2 v2.11.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.12.3 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	go.opencensus.io v0.24.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
--- main.go
+++ main.go
@@ -12,11 +12,18 @@ import (
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/messaging"
+	"github.com/sideshow/apns2"
+	"github.com/sideshow/apns2/token"
+	badger "github.com/dgraph-io/badger/v4"
+	"github.com/go-co-op/gocron"
 )
 
-var app *firebase.App
+var apnClient *apns2.Client
+var apnTopic string
+var fbClient *messaging.Client
 var ctx context.Context
-var client *messaging.Client
+var db *badger.DB
+var ticker *time.Ticker
 
 type Response struct {
 	Message string `json:"message"`
@@ -30,6 +37,12 @@ type Notification struct {
 	FromUri string `json:"from-uri"`
 	DisplayName string `json:"display-name"`
 }
+
+type Registration struct {
+	Token string `json:"token"`
+	Topic string `json:"topic"`
+}
+
 
 func ping(c *gin.Context) {
 	c.JSON(http.StatusOK, Response{Message: "pong"})
@@ -46,6 +59,49 @@ func audit(result string, response string, notification *Notification) {
 	w.Flush()
 }
 
+
+func register(c *gin.Context) {
+	var registration Registration
+
+	if err := c.BindJSON(&registration); err != nil {
+		c.JSON(http.StatusInternalServerError, Response{Message: "Invalid parameters"})
+		return
+	}
+	
+	// Send a background silent notification to verify if the device token is valid
+	notification := &apns2.Notification{}
+	notification.DeviceToken = registration.Token
+	notification.Topic = apnTopic
+	notification.PushType = apns2.PushTypeBackground
+	notification.Payload = []byte(`{
+			"aps" : {
+				"badge" : 0,
+				"content-available": 1
+			}
+		}
+	`)
+	res, err := apnClient.Push(notification)
+
+	if err != nil || res.StatusCode != 200 {
+		c.JSON(http.StatusInternalServerError, Response{Message: res.Reason})
+		return
+	}
+
+	// If the token is valid store <topic>:<device> key/value
+	// Set a TTL of one year, the TTL is updated on each use
+	dberr := db.Update(func(txn *badger.Txn) error {
+		record := badger.NewEntry([]byte(registration.Topic), []byte(registration.Token)).WithTTL(24 * 365 * time.Hour)
+		err := txn.SetEntry(record)
+		return err
+	})
+
+	if dberr != nil {
+		c.JSON(http.StatusInternalServerError, Response{Message: dberr.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, Response{Message: "success"})
+}
+
 func send(c *gin.Context) {
 	var notification Notification
 	var message *messaging.Message
@@ -56,91 +112,195 @@ func send(c *gin.Context) {
 	}
 
 	if notification.Type == "apple" {
-		badge := 0
-		alert := &messaging.ApsAlert {
-			Title: notification.CallId,
-			Body: notification.Uuid,
-			LocKey: "call",
+		var deviceToken []byte
+		err := db.View(func(txn *badger.Txn) error {
+			item, err := txn.Get([]byte(notification.Topic))
+
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, Response{Message: "No topic found"})
+				return err
+			}
+			ierr := item.Value(func(val []byte) error {
+				deviceToken = make([]byte, len(val))
+				copy(deviceToken, val)
+				return nil
+			})
+			return ierr
+		})
+		
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, Response{Message: "No device token found"})
+			return
 		}
-		payload := &messaging.APNSPayload {
-			Aps: &messaging.Aps{
-				Badge: &badge,
-				ContentAvailable: true,
-				CustomData: map[string]interface{}{
-			//		"loc-key": "call",
-			//		"loc-args": []string{},
-					"call-id": notification.CallId,
-					"uuid": notification.Uuid,
-					"send-time": uint(time.Now().Unix()),
-				},
-				Alert: alert,
+		apnNotif := &apns2.Notification{}
+		apnNotif.DeviceToken = string(deviceToken)
+		apnNotif.Topic = apnTopic
+		apnNotif.PushType = apns2.PushTypeVOIP
+		apnNotif.Payload = []byte(fmt.Sprintf(`{
+			"aps" : {
+				"sound": "",
+				"call-id": "%s",
+				"uuid": "%s",
+				"send-time": "%s"
 			},
-			CustomData: map[string]interface{}{
-				"from-uri": notification.FromUri,
-				"display-name": notification.DisplayName,
-				"pn_ttls": 0,
-				"customPayload": struct{}{},
-			},
+			"from-uri": "%s",
+			"display-naame": "%s",
+			"pn_ttl": 100,
+			"customPayload": {}
+		}
+		`, notification.CallId, notification.Uuid, time.Now().Format("2006-01-02 12:32:06"), notification.FromUri, notification.DisplayName))
+		// Send voip notification
+		res, err := apnClient.Push(apnNotif)
+		if res == nil {
+			c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
+			audit("error", err.Error(), &notification)
+			return
 		}
 
-		message = &messaging.Message{
-			Data: map[string]string{ "call-id": notification.CallId, "uuid": notification.Uuid },
-			APNS: &messaging.APNSConfig{
-				Headers: map[string]string{
-					"apns-priority": "10",
-					"apns-push-type": "voip",
-					"apns-topic": "it.nethesis.nethcti3.voip",
-				},
-				Payload: payload,
-			},
-			Topic: notification.Topic,
+		if res.StatusCode != 200 {
+			c.JSON(http.StatusInternalServerError, Response{Message: res.Reason})
+			audit("error", res.Reason, &notification)
+			return
 		}
+
+		// Extend topic expiration
+		// Ignore error here. Worst case: the topic expires after one year
+		db.Update(func(txn *badger.Txn) error {
+			record := badger.NewEntry([]byte(notification.Topic), []byte(deviceToken)).WithTTL(24 * 365 * time.Hour)
+			err := txn.SetEntry(record)
+			return err
+		})
+
+		audit("success", res.ApnsID, &notification)
+		c.JSON(http.StatusOK, Response{Message: res.ApnsID})
+		return
 	} else if notification.Type == "firebase" {
 		message = &messaging.Message{
 			Data: map[string]string{ "call-id": notification.CallId, "uuid": notification.Uuid },
 			Topic: notification.Topic,
 		}
+		response, err := fbClient.Send(ctx, message)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
+			audit("error", err.Error(), &notification)
+			return
+		}
+
+		audit("success", response, &notification)
+		c.JSON(http.StatusOK, Response{Message: response})
+		return
 	} else {
 		c.JSON(http.StatusInternalServerError, Response{Message: "Invalid notification type"})
 		return
 	}
-	response, err := client.Send(ctx, message)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
-		audit("error", err.Error(), &notification)
-		return
-	}
-
-	audit("success", response, &notification)
-	c.JSON(http.StatusOK, Response{Message: response})
 }
 
-func initFirebase() (app *firebase.App) {
-	var err error
-	app, err = firebase.NewApp(context.Background(), nil)
+
+func initFirebase() {
+	var fberr error
+	app, err := firebase.NewApp(context.Background(), nil)
 
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error initializing app: ", err.Error())
+		fmt.Fprintln(os.Stderr, "Error initializing Firebase app: ", err.Error())
 		os.Exit(1)
 	}
         ctx = context.Background()
-	client, err = app.Messaging(ctx)
+	fbClient, fberr = app.Messaging(ctx)
 
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error initializing messaging client: ", err.Error())
+	if fberr != nil {
+		fmt.Fprintln(os.Stderr, "Error initializing Firebase messaging client: ", err.Error())
+		os.Exit(2)
+	}
+}
+
+func initAPN() {
+	p8 := os.Getenv("APPLE_APPLICATION_CREDENTIALS")
+	if len(p8) == 0 {
+		fmt.Fprintln(os.Stderr, "Error initializing APN client: no p8 file given")
+		os.Exit(1)
+	}
+	authKey, terr := token.AuthKeyFromFile(p8)
+	if terr != nil {
+		fmt.Fprintln(os.Stderr, "Error initializing APN auth: ", terr.Error())
 		os.Exit(2)
 	}
 
-	return app
+	keyId := os.Getenv("APPLE_KEY_ID")
+	if len(keyId) == 0 {
+		fmt.Fprintln(os.Stderr, "Error initializing APN key id: empty key id")
+		os.Exit(3)
+	}
+	teamId := os.Getenv("APPLE_TEAM_ID")
+	if len(teamId) == 0 {
+		fmt.Fprintln(os.Stderr, "Error initializing APN team id: empty team id")
+		os.Exit(4)
+	}
+
+	apnTopic = os.Getenv("APPLE_TOPIC")
+	if len(apnTopic) == 0 {
+		fmt.Fprintln(os.Stderr, "Error initializing APN topic: empty topic")
+		os.Exit(4)
+	}
+
+	apnEnv := os.Getenv("APPLE_ENVIRONMENT")
+	if len(apnEnv) == 0 {
+		fmt.Fprintln(os.Stderr, "Error initializing APN env: empty environment")
+		os.Exit(4)
+	}
+
+	token := &token.Token{
+		AuthKey: authKey,
+ 		KeyID:   keyId,
+		TeamID:  teamId,
+	}
+
+	if apnEnv == "sandbox" {
+		apnClient = apns2.NewTokenClient(token).Development()
+	} else if apnEnv == "production" {
+		apnClient = apns2.NewTokenClient(token).Production()
+	} else {
+		fmt.Fprintln(os.Stderr, "Error initializing APN env: invalid env value ", apnEnv)
+		os.Exit(4)
+	}
 }
 
+func initDB() (* badger.DB){
+	dbPath := os.Getenv("DB_PATH")
+	if len(dbPath) == 0 {
+		fmt.Fprintln(os.Stderr, "Error initializing DB: invalid db path")
+		os.Exit(4)
+	}
+
+	db, err := badger.Open(badger.DefaultOptions(dbPath))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error initializing DB: ", err.Error())
+		os.Exit(5)
+	}
+
+	// Setup db cleanup job
+	cleanupJob := gocron.NewScheduler(time.UTC)
+	cleanupJob.Every(2).Seconds().Do(func() {
+		err := db.RunValueLogGC(0.5)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error on DB cleanup: ", err.Error())
+		}
+	})
+	return db
+}
 
 func main() {
+	// Connect to Firebase for Android notifications
 	initFirebase()
+	// Connect to Apple APN for iOS notifications
+	initAPN()
+	// Initialize DB to store iOS device token
+	db = initDB()
+	defer db.Close()
 
 	router := gin.Default()
 	router.GET("/ping", ping)
 	router.POST("/send", send)
+	router.POST("/register", register)
 
 	listen := os.Getenv("LISTEN")
 	if len(listen) == 0 {
